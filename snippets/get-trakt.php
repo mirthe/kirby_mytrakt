63b830139fcb9e9efbce651d9e0eace7e24706d0
Mirthe Valentijn <me@mirthe.org>
2024-04-04 18:17:24 +0200

    Increase frequency, change syntax

--- snippets/get-trakt.php
+++ snippets/get-trakt.php
@@ -1,4 +1,5 @@
-<?php if (!file_exists($localfile) OR time()-filemtime($localfile) > 2 * 3600 OR isset($_GET['forcecache'])) {
+<?php if (!file_exists($localfile) || time()-filemtime($localfile) > 1800 || isset($_GET['forcecache'])) {
+// when file is not available, older than 30 minutes, or forced
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $feedurl);
@@ -19,8 +20,8 @@
     // echo '<p>Nieuwe feed opgehaald.</p>';
 
     // TODO cache opschonen voor deze pagina, maar hoe is die syntax!?
-    // $kirby->cache('page')->series()->flush(); 
+    // $kirby->cache('page')->series()->flush();
 
 } else {
-    $feed = file_get_contents($localfile); 
-} ?>
+    $feed = file_get_contents($localfile);
+}
